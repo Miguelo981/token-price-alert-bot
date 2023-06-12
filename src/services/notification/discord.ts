1d5598f9d335a4fa32911db1a5472d35c7e4d030
Miguelo981 <55580212+Miguelo981@users.noreply.github.com>
2023-06-12 21:35:43 +0200

    Add discord bot integration

---
+++ src/services/notification/discord.ts
@@ -0,0 +1,38 @@
+import { Client, GatewayIntentBits, Partials } from "discord.js"
+import { DISCORD_BOT_TOKEN, DISCORD_CHANNEL_ID } from "../../constants.js"
+
+let client: Client
+
+export async function initClient() {
+  if (client) return
+
+  client = new Client({
+    intents: [
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.Guilds
+    ],
+    partials: [
+      Partials.Message,
+      Partials.Channel,
+    ]
+  })
+
+  await new Promise((resolve) => {
+    client.on("ready", () => {
+      console.log(`Logged in as ${client.user.tag}!`)
+      resolve(null)
+    })
+
+    client.login(DISCORD_BOT_TOKEN)
+  })
+}
+
+export async function sendChannelMessage(message: string) {
+  const channel = await client.channels.cache.get(DISCORD_CHANNEL_ID)
+  
+  if (!channel) return
+  
+  (channel as any).send(message)
+}
